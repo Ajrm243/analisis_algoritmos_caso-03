1bde71d10185fc1ada94e5214c12019aa7da4df4
Adrian Ramirez Mattey <aramirezmattey@gmail.com>
2022-04-08 17:59:13 -0600

    preparar un poco la estructura

---
+++ code/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include "headers/observers.hpp"
+
+int main() {
+    return 0;
+}
---
+++ headers/observers.hpp
@@ -0,0 +1,15 @@
+class Observer {
+    public:
+        Observer(){};
+        virtual ~Observer(){};
+        virtual void update(void* element) = 0;
+};
+
+class Subject {
+    public:
+        Subject(){};
+        virtual ~Subject(){};
+        virtual void attach(Observer* observer) = 0;
+        virtual void dettach(Observer* observer) = 0;
+        virtual void notify(void* element) = 0;
+};
