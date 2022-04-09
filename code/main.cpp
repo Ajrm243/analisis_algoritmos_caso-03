5b54f8cd8c93e315ba05d5e1c05067561d9856fe
Adrian Ramirez Mattey <aramirezmattey@gmail.com>
2022-04-08 19:10:42 -0600

    algunas pruebas

--- code/main.cpp
+++ code/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <list>
 #include <bits/stdc++.h>
 #include "headers/observers.hpp"
+
+//using namespace std;
+class Operator : public Subject{
+
+    private:
+    std::list<Observer*> ObserverList;
+
+    public:
+
+    void attach(Observer* pObserver) {
+        ObserverList.push_back(pObserver);
+    }
+
+    void detach(Observer* pObserver) {
+        ObserverList.remove(pObserver);
+    }
+
+};
+
+
+class Selector : public Observer {
+    
+};
 
 int main() {
     return 0;
--- headers/observers.hpp
+++ headers/observers.hpp
@@ -10,6 +10,6 @@ class Subject {
         Subject(){};
         virtual ~Subject(){};
         virtual void attach(Observer* observer) = 0;
-        virtual void dettach(Observer* observer) = 0;
+        virtual void detach(Observer* observer) = 0;
         virtual void notify(void* element) = 0;
 };
