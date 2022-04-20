7a4e897ad077edcec753022dd30cb569f0b8e054
María José Porras <marijopm27@estudiantec.cr>
2022-04-20 13:54:51 -0600

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <list>
 #include <bits/stdc++.h>
 #include "headers/observers.hpp"
+
+//using namespace std;
+class Operator : public Subject{
+
+    private:
+    std::list<Observer*> ObserverList;
+
+    public:
+
+    void attach(Observer* pObserver) {
+        ObserverList.push_back(pObserver);
+    }
+
+    void detach(Observer* pObserver) {
+        ObserverList.remove(pObserver);
+    }
+
+};
+
+
+class Selector : public Observer {
+
+};
+
+class Router : public Observer {
+
+};
+
+class Generator : public Observer {
+
+};
 
 int main() {
     return 0;
