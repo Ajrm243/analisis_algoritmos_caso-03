3c6b5a4ab085605e04aaa686a633e383bca2ac4e
María José Porras <marijopm27@estudiantec.cr>
2022-04-24 01:30:06 -0600

    Reconocimiento de paths y colores
    Unicamente se hizo el reconocimiento de los paths y de los colores. Aún hace falta parsear el "d" de los paths, para analizar los puntos y posiciones

---
+++ code/Seleccion.hpp
@@ -0,0 +1,74 @@
+#include "main.hpp"
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include <sstream>
+#include <fstream>
+
+//Lista de colores dadas por el usuario
+list <string> lista_colores = {"red","yellow"};
+
+//Recorrer y reconocer los paths
+void extractData(xml_node<>* node){
+    //Reconocer con base en la etiqueta del nodo si los node_element "path" y "g" --> Por el momento
+    //Por el momento unicamente se estan imprimiendo hasta definir la estructura que se utilizara para almacenarlas
+    for (node = node->first_node(); node != NULL; node = node->next_sibling()){
+        
+        if (node->type() == node_element){
+        string etiqueta = node->name();
+        string relleno, color;
+        if (etiqueta == "path"){
+
+            cout << "Etiqueta: " << node->name() << endl;
+            for (xml_attribute<>* attrib = node->first_attribute(); attrib != NULL; attrib = attrib->next_attribute()){
+                relleno = attrib->name();
+                if (relleno == "stroke"){
+                    color  = attrib->value();           
+                    list<string>::iterator findIter = find(lista_colores.begin(), lista_colores.end(), color);
+                    if(findIter != lista_colores.end()){
+                        cout<<"Coincidencia encontrada"<<endl;
+                    }
+                }
+                }
+        }
+        
+        if (etiqueta == "g")
+        {
+            cout << "Etiqueta: " << node->name() << endl;
+            for (xml_attribute<>* attrib = node->first_attribute(); attrib != NULL; attrib = attrib->next_attribute()){
+                relleno = attrib->name();
+                if (relleno == "fill"){
+                    cout << " Atributo: " << attrib->name() << endl;
+                    cout << "\tValor: " << attrib->value() << endl;
+                    
+                    color  = attrib->value();
+                
+                    list<string>::iterator findIter = find(lista_colores.begin(), lista_colores.end(), color);
+                    if(findIter != lista_colores.end()){
+                        cout<<"Coincidencia encontrada"<<endl;
+                    }
+                    
+                }
+                }
+        }
+        extractData(node);
+        }
+        
+        }
+    }
+
+
+void extractPath(xml_document<>* doc){
+    xml_node<>* node = doc->first_node();
+
+    cout << "Etiqueta: " << node->name() << endl;
+    cout<< node->name()<<endl;
+    for (xml_attribute<>* attrib = node->first_attribute(); attrib != NULL; attrib = attrib->next_attribute()){
+        //Reconocer y guardar valores del svg como el height y el width
+        cout << " Atributo: " << attrib->name() << endl;
+        cout << "\tValor: " << attrib->value() << endl;
+    }
+
+    extractData(node);
+    }
+
--- code/main.cpp
+++ code/main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 //#include "headers/observers.hpp"
 #include "main.hpp"
+#include "Seleccion.hpp"
 
 //Recorre el elemento raíz del documento
 void extractXMLData(xml_document<>* doc){
@@ -35,46 +36,8 @@ void extractNodeData(xml_node<>* node){
 }
 
 
-//Recorrer y Extraer los paths
 
-void extractData(xml_node<>* node){
 
-  for (node = node->first_node(); node != NULL; node = node->next_sibling()){
-    if (node->type() == node_element){
-      string etiqueta = node->name();
-      if (etiqueta == "path"){
-        cout << "Etiqueta: " << node->name() << endl;
-        for (xml_attribute<>* attrib = node->first_attribute(); attrib != NULL; attrib = attrib->next_attribute()){
-          cout << "\tAtributo: " << attrib->name() << endl;
-          cout << "\t-Valor: " << attrib->value() << endl;
-        }
-      }
-
-      extractData(node);
-    }
-  }
-}
-
-void extractPath(xml_document<>* doc){
-  xml_node<>* node = doc->first_node();
-
-  cout << "Etiqueta: " << node->name() << endl;
-  cout<< node->name()<<endl;
-  for (xml_attribute<>* attrib = node->first_attribute(); attrib != NULL; attrib = attrib->next_attribute()){
-    cout << " Atributo: " << attrib->name() << endl;
-    cout << "\tValor: " << attrib->value() << endl;
-  }
-
-  extractData(node);
-}
-
-inline string attr_value_or_default(rapidxml::xml_attribute<>* attr, string default_value="") {
-    if(attr == nullptr) {
-        return default_value;
-    } else {
-        return attr->value();
-    }
-}
 
 /*
 
