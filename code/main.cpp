2381158bdc89a3970023b867079b1ebfdf46125f
María José Porras <marijopm27@estudiantec.cr>
2022-04-25 18:32:08 -0600

    Mejor organizaci[on

--- code/main.cpp
+++ code/main.cpp
@@ -1,7 +1,11 @@
+#include <iostream>
+#include <list>
+//#include <bits/stdc++.h>
+#include "../rapidxml/rapidxml_ext.hpp" //Clases para manejo del DOM
+#include "../rapidxml/rapidxml_utils.hpp" //Clase File
+#include "../headers/main.hpp"
 #include "../headers/observers.hpp"
-#include "main.hpp"
 #include "../headers/Seleccion.hpp"
-
 
 
 /*
@@ -12,8 +16,7 @@
 int frames, angulo, height, width;
 list <xml_node<>* > selected_paths; //Unicamente agregar los node_element con etiquetas"path" y/o "g"
 
-vector <Path> listaPathRecolectados ;
-vector <Path> listaPathSeleccionados ;
+
 
 
 //Recorre el elemento raíz del documento
@@ -46,47 +49,36 @@ void extractNodeData(xml_node<>* node){
 }
 
 /*
-
 //using namespace std;
 class Operator : public Subject{
-
     private:
     std::list<Observer*> ObserverList;
-
     public:
-
     void attach(Observer* pObserver) {
         ObserverList.push_back(pObserver);
     }
-
     void detach(Observer* pObserver) {
         ObserverList.remove(pObserver);
     }
-
 };
-
-
 class Selector : public Observer {
-
 };
-
 class Router : public Observer {
-
 };
-
 class Generator : public Observer {
-
 };
 */
 int main() {
     //Leer XML
-    file<> file("ejemplos_svg/EjemploSimple1.svg"); // Lee y carga el archivo en memoria
+    file<> file("../svganimation/images/svg/wifi-2.svg"); // Lee y carga el archivo en memoria
     xml_document<> myDoc; //Raíz del árbol DOM
     myDoc.parse<0>(file.data()); //Parsea el XML en un DOM
 
     //Recorrer elementos y atributos
     //extractXMLData(&myDoc);
-    xml_node<> *root = myDoc.first_node("svg");
+    xml_node<> *root = myDoc.first_node("g");
+
+    recolectarPaths(root);
     // Get root node
 
     cout << endl;
@@ -94,9 +86,7 @@ int main() {
 
     //Modificar un atributo existente
     //Modifica el atributo indicado del primer elemento <path> que se encuentre
-    //xml_node<> *modifyNode = myDoc.first_node()->first_node("path");
-    
-
+    //xml_node<> *modifyNode = myDoc.first_node()->first_node("path")
 
     return 0;
 }
--- headers/Seleccion.hpp
+++ headers/Seleccion.hpp
@@ -1,4 +1,5 @@
-#include "../code/main.hpp"
+#include "../headers/main.hpp"
+#include "../headers/observers.hpp"
 
 /*
     *****Posible acomodo de la clase Seleccion ******
@@ -21,8 +22,10 @@ public:
         else if (value == 2)
             cout << "Analizando intersecciones de puntos con valores de paths" << endl;
     }
-} ;
+};
 
+extern vector <Path> capturedPathList;
+extern vector <Path> selectedPathList;
 
 //Lista de colores dadas por el usuario
 list <string> lista_colores = {"red","yellow"};
@@ -46,7 +49,7 @@ void recolectarPaths(xml_node<>* node){
             d =  node->first_attribute("d")->value();
             path.setPath(d);
 
-            listaPathRecolectados.push_back(path);
+            capturedPathList.push_back(path);
         }
         
         }
@@ -59,94 +62,70 @@ void recolectarPaths(xml_node<>* node){
 void FiltrarColor(){
     int numeroPath;
     string color;
-    for (numeroPath=0; numeroPath<listaPathRecolectados.size(); numeroPath++){
-        Path path = listaPathRecolectados.at(numeroPath);
+    for (numeroPath=0; numeroPath<capturedPathList.size(); numeroPath++){
+        Path path = capturedPathList.at(numeroPath);
         color = path.getColor();
         list<string>::iterator findIter = find(lista_colores.begin(), lista_colores.end(), color);
             if(findIter != lista_colores.end()){
                 //Seleccionar path para la siguiente fase de seleccion
                 cout<<"Coincidencia encontrada"<<endl; //Hacer el append del elemenot path
-                listaPathSeleccionados.push_back(path);
+                selectedPathList.push_back(path);
             }
     }
 }
 
 
 /*
-
 Analisis de los elementos del svg 
-
 *****************
 *   Rectangle   *
 *****************
 Manera1
 <rect x="10" y="10" width="30" height="30"/>
-
 Manera2
 <rect x="60" y="10" rx="10" ry="10" width="30" height="30"/>
-
 Manera3
 <rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
-
-
 *****************
 *    Circle     *
 *****************
 Manera1
 <circle cx="25" cy="75" r="20" fill="red"/>    --> r=radio, cy & cx punto central del circulo
-
-
 *****************
 *    Ellipse    *
 *****************
 Manera1
 <ellipse cx="75" cy="75" rx="20" ry="5"/>
-
 Manera2
  <ellipse cx="200" cy="80" rx="100" ry="50" style="fill:yellow;stroke:purple;stroke-width:2" />  --> etiqueta "style" para relleno
-
     -rx = radio horizontal
     -ry = radio vertical
     -cx & cy = punto central del elipse
-
-
 *****************
 *      Line     *
 *****************
 Manera1
 <line x1="10" x2="50" y1="110" y2="150" stroke="black" stroke-width="5"/>
-
-
 *****************
 *    Polygon    *
 *****************
 Manera1
 <polygon points="200,10 250,190 160,210" style="fill:lime;stroke:purple;stroke-width:1" />
-
 Grupos de puntos separados por un espacio
-
-
 *****************
 *    Polyline   *
 *****************
 Manera1
 <polyline points="60, 110 65, 120 70, 115 75, 130 80, 125 85, 140 90, 135 95, 150 100, 145"/>
-
 Grupos de puntos separados por un espacio
-
-
 *****************
 *      Path     *
 *****************
     @ AYUDA 
         Visualizador de paths = https://svg-path-visualizer.netlify.app/
-
     Atributo a buscar = "d"  --> define el trazado que va a dibujarse
-
     Comandos de ruta de acceso:
-
             ~MOVE TO  (M, m):
-
                 M = M 250 50 
                     Coordenadas donde empieza el trazado.
                 m = m  0,10
@@ -154,68 +133,46 @@ Grupos de puntos separados por un espacio
                     largo del eje x y a lo largo del eje y"
             
             ~LINE TO (L,l,H,h,V,v):
-
                 L, V, H --> Utilizan coordenadas absolutas
-
                 L = L 90,90
                     Coloque el lápiz y dibuje una línea a { x: 90, y: 90 }
-
                 V = V 10
                     Mover verticalmente a 10
-
                 H = H 50
                     Mover horizontalmente a 50
-
                 l,v,h --> utilizan coordenadas relativas
                 
                 L = L 80,80
                     Coloque el bolígrafo y mueva a la derecha 80 y 80 inferior desde la posición actual
-
                 V = V -80
                     Mover el top 80 de la posición actual
-
                 H = H -40
                     Mover a la izquierda 40 de la posición actual
             
             ~Curva cúbica de Bézier (C,c,S,s):
-
                 C & S --> Utilizan coordenadas absolutas
-
                 C = C 30,90 25,10 50,10
                     Trazar una linea curva que involucre los puntos
-
                 S = S 70,90 90,90
                     Suavizar la curva atravesando los puntos
-
                 c & s --> Utilizan coordenadas relativas
-
                 c = c 20,0 15,-80 40,-80
                     Coloque el lápiz y dibuje una curva de Bézier desde el punto actual hasta un nuevo punto { x: punto anterior + 40, y: punto anterior - 80 }
-
                 s = s 20,80 40,80
                     Dibujar una curva de Bézier suave desde el punto actual hasta un nuevo punto { x: punto anterior + 40, y: punto anterior + 80 }
                     
             ~Curva cuadrática de Bézier (Q,q, T,t):
-
                 Q & T --> Utilizan coordenadas absolutas
-
                 Q 25,25 40,50
                     Coloque el lápiz y dibuje una curva de Bézier cuadrática desde el punto actual hasta un nuevo punto { x: 40, y: 50 }
-
                     El punto de control es { x: 25, y: 25 }  
                 
-
                 q & t --> Utilizan coordenadas relativas
-
                 t 30,0 30,0 30,0 30,0 30,0
                 t 30,0
                 Dibujar una curva de Bézier cuadrática desde el punto actual hasta un nuevo punto { x: punto anterior + 30, y: punto anterior + 0 }
                 El punto de control es el reflejo del punto de control final del comando de curva anterior
-
-
 *************************
 *     SWITCH DE CASO    *
 *************************
-
-*/    
-
+*/
