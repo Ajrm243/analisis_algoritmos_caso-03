3fb5b5cecf97910329996728b2c0f9b19b4c5ef1
María José Porras <marijopm27@estudiantec.cr>
2022-04-27 14:58:56 -0600

    Revert "Correcting errors in merge"
    This reverts commit b42642aa7f843b837d586d67db3c90cce36446d6.

--- code/main.cpp
+++ code/main.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
+##include <iostream>
 #include <list>
-#include <bits/stdc++.h>
 #include "../rapidxml/rapidxml_ext.hpp" //Clases para manejo del DOM
 #include "../rapidxml/rapidxml_utils.hpp" //Clase File
 #include "../headers/main.hpp"
