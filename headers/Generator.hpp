6ac48b08626bac38afc72364763f8b8a7ae6423f
María José Porras <marijopm27@estudiantec.cr>
2022-05-08 15:53:57 -0600

    Update Generator.hpp

--- headers/Generator.hpp
+++ headers/Generator.hpp
@@ -2,71 +2,58 @@
 #define GENERATOR_H
 #include "../headers/main.hpp"
 
-void writeSVG(){
-    //Escribir un nuevo archivo
-  file<> file("wifi-1.svg"); // Lee y carga el archivo en memoria
-  xml_document<> myDoc; //Raíz del árbol DOM
-  myDoc.parse<0>(file.data()); //Parsea el XML en un DOM
+using namespace std;
+class Generator : public Observer {
+    private:
+    // ratio es un numero arbitrario que más o menos expresa qué tan grande es el canvas
+    double ratio;
+    double canvasHeight;
+    double canvasWidth;
+    vector<Path> selectedPathList;
+    // {R,G,B}, {R,G,B}
+    vector<vector<int>> allColorsByChannel;
+    public:
+        Generator() {}
+        ~Generator() {}
+        // https://www.plantuml.com/plantuml/svg/PP31JiCm44Jl_efHBgI7_a0aAY5GkQ1o27o0usoe8ROZUmD4LVyTjwcRfFXcUtPsvh7cbl1Q1VBVv94_k-rAX2zjE-cbGU0XVWVsMd5lMd4tVLd7j7MMTz8GNdgbQubIse2teQCVhvcysEkQGARZ0OPCHJu8MKKtgHYgbI6WxAjFYhU3U62MQf-dA2lnH3SFEyUw6VALE0fnsewM6ePhaabHZ0hHE0_Pjd3G5kuo7-8EmAqtxrqjcV8LZe9iVUuXXBOnRoFLZ4MK7rBQ8zBKQNaAdLszCwmtqsI13DUJ2J5MVNHMUM9Aq_JBSVyS0V1CNFAu1oZdbp2gpW__0DnO5yNC9xxttYxdfzvZj2mPf0ePP82A_4pNaY7BOS5bee4htsKohp_svsZA5SMe5WibxgTIwyqixooRGtPHHBwdhX7oBSK5Qyon_m40
+        // segun esto los observers tambien pueden tener la referencia del subject
+        void update(void* pUserPacket) {
+            infoPacket* castedPacket = (static_cast<infoPacket*>(pUserPacket));
+            update(castedPacket);
+        }
+        void update(infoPacket* pUserPacket) {
+            /*
+            * en el packet interesa:
+            * 1- nodo xml (padre) del svg
+            * 2- lista de colores del usuario
+            * 3- lista de puntos a buscar aproximar
+            */
 
-  ofstream copyFile("sample2.svg"); //Nuevo archivo
-  stringstream ss;
-  ss << *myDoc.first_node(); //Pasa el nodo raíz a ss
-  string stringXML = ss.str(); //ss.toString
-  copyFile << stringXML;
-  copyFile.close(); //Escribe y cierra
-}
+        }
+        void generate(){
+          
+        }
+        void writeSVG(){
+            //Escribir un nuevo archivo
+          file<> file("wifi-1.svg"); // Lee y carga el archivo en memoria
+          xml_document<> myDoc; //Raíz del árbol DOM
+          myDoc.parse<0>(file.data()); //Parsea el XML en un DOM
 
-int mainDouble(){
-  /* DOM
-    Transformación de un XML en un árbol donde cada elemento es un nodo. Cada nodo puede tener un valor y 
-  una lista de atributos. A su vez, cada atributo tiene un valor.
-  */
+          ofstream copyFile("sample2.svg"); //Nuevo archivo
+          stringstream ss;
+          ss << *myDoc.first_node(); //Pasa el nodo raíz a ss
+          string stringXML = ss.str(); //ss.toString
+          copyFile << stringXML;
+          copyFile.close(); //Escribe y cierra
+        }
 
-  //Leer XML
-  file<> file("sample.svg"); // Lee y carga el archivo en memoria
-  xml_document<> myDoc; //Raíz del árbol DOM
-  myDoc.parse<0>(file.data()); //Parsea el XML en un DOM
+        void produce(){
 
-  //Recorrer elementos y atributos
-  writeSVG();
-  extractXMLData(&myDoc);
+        }
 
-  //Modificar un atributo existente
-  //Modifica el atributo indicado del primer elemento <path> que se encuentre
-  xml_node<> *modifyNode = myDoc.first_node()->first_node("path");
+        void consumer(){
 
-  string newDirection = "M 10 10 L 50 50 L 10 50 Z";
-  modifyNode->first_attribute("d")->value(newDirection.c_str());
+        }
 
-  string newColor = "pink";
-  modifyNode->first_attribute("stroke")->value(newColor.c_str());
-
-  modifyNode->next_sibling("g")->first_attribute("stroke")->value(newColor.c_str());
-
-  //Insertar un nuevo elemento
-  xml_node<> *newNode = myDoc.allocate_node(node_element, "path");
-  myDoc.first_node()->append_node(newNode); //Elemento <path>
-
-  xml_attribute<> *newAttr = myDoc.allocate_attribute("d", "M 0 0 L 0 15 L 20 15 L 20 20 Z");
-  newNode->append_attribute(newAttr); //Atributo "d" para <path>
-
-  xml_attribute<> *newAttr2 = myDoc.allocate_attribute("fill", "green");
-  newNode->append_attribute(newAttr2); //Atributo "fill" para <path>
-
-  //Escribir un nuevo archivo
-  /*
-  ofstream copyFile("sample2.svg"); //Nuevo archivo
-  stringstream ss;
-  ss << *myDoc.first_node(); //Pasa el nodo raíz a ss
-  string stringXML = ss.str(); //ss.toString
-  copyFile << stringXML;
-  copyFile.close(); //Escribe y cierra
-*/
-  //Document to String
-  string strXML;
-  print(back_inserter(strXML), myDoc, 0); //Copia el texto del Document en la variable
-  cout << strXML << endl;
-
-  return 0;
-}
+};
 #endif
